e17152dca6f48558376cb0f4f5f3c7fb32c7129b
Aleksei Vetrov <vvvvvv@google.com>
2024-10-29 13:22:11 +0000

    wifi: nl80211: fix bounds checker error in nl80211_parse_sched_scan
    The channels array in the cfg80211_scan_request has a __counted_by
    attribute attached to it, which points to the n_channels variable. This
    attribute is used in bounds checking, and if it is not set before the
    array is filled, then the bounds sanitizer will issue a warning or a
    kernel panic if CONFIG_UBSAN_TRAP is set.
    
    This patch sets the size of allocated memory as the initial value for
    n_channels. It is updated with the actual number of added elements after
    the array is filled.
    
    Fixes: aa4ec06c455d ("wifi: cfg80211: use __counted_by where appropriate")
    Cc: stable@vger.kernel.org
    Signed-off-by: Aleksei Vetrov <vvvvvv@google.com>
    Signed-off-by: GuidixX <guidixm@gmail.com>

--- net/wireless/nl80211.c
+++ net/wireless/nl80211.c
@@ -9578,6 +9578,7 @@ nl80211_parse_sched_scan(struct wiphy *wiphy, struct wireless_dev *wdev,
 			+ ie_len, GFP_KERNEL);
 	if (!request)
 		return ERR_PTR(-ENOMEM);
+	request->n_channels = n_channels;
 
 	if (n_ssids)
 		request->ssids = (void *)request +
